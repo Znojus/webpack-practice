27305a8c85701e27d4db3e2e47ccd224d794343e
Znojus <150673971+Znojus@users.noreply.github.com>
2026-05-31 13:25:40 +0300

    Set up second folder for another tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ export default {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
         ],
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
     },
 };
